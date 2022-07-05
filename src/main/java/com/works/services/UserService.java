44aa6325de78614430724698305a6b0fa09f1caf
isa Eren <105389265+Eren100@users.noreply.github.com>
2022-07-05 21:41:14 +0300

    service start

--- src/main/java/com/works/services/UserService.java
+++ src/main/java/com/works/services/UserService.java
@@ -1,7 +1,43 @@
 package com.works.services;
 
+import com.works.entities.User;
+import com.works.repositories.UserRepository;
+import com.works.utils.ERest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
+final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+     public ResponseEntity save(User user) {
+         Map<ERest,Object> hm=new LinkedHashMap<>();
+         return new ResponseEntity(hm, HttpStatus.OK);
+    }
+
+     public ResponseEntity login(User user) {
+         Map<ERest,Object> hm=new LinkedHashMap<>();
+         return new ResponseEntity(hm, HttpStatus.OK);
+    }
+
+     public ResponseEntity register(User user) {
+         Map<ERest,Object> hm=new LinkedHashMap<>();
+         return new ResponseEntity(hm, HttpStatus.OK);
+    }
+
+      public ResponseEntity search(String q) {
+          Map<ERest,Object> hm=new LinkedHashMap<>();
+          return new ResponseEntity(hm, HttpStatus.OK);
+    }
 }
